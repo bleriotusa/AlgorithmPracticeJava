4394f9af1877f6c9571747a6193df765beda5eea
Michael J. Chen <bleriotusa@gmail.com>
2016-03-12 22:35:25 -0800

    Finish work on ClosestBSTValue.

--- src/ClosestBinarySearchTreeValue/ClosestBSTValue.java
+++ src/ClosestBinarySearchTreeValue/ClosestBSTValue.java
@@ -13,53 +13,43 @@ class TreeNode {
     TreeNode(int x) {
         val = x;
     }
+
+    TreeNode(int x, TreeNode left, TreeNode right) {
+        val = x;
+        this.left = left;
+        this.right = right;
+    }
 }
 
 
 public class ClosestBSTValue {
 
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(2147483647);
-//        root.left = new TreeNode(2);
-//        root.right = new TreeNode(7);
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+        root.right = new TreeNode(5);
         ClosestBSTValue answer = new ClosestBSTValue();
-        System.out.println(answer.closestValue(root, 0.0));
+        System.out.println(answer.closestValue(root, 3.78));
     }
-    double lastValue = Double.MAX_VALUE;
-    boolean left = false;
 
+    double bestValue = Double.MAX_VALUE;
+
+    /**
+     * Do a binary search, keeping track of the closest value to the target on the log(n) path
+     * Guaranteed to be on the search path, because we always search to get closer to the target.
+     * <p>
+     * bestValue calculation = (distance from target to root < distance from target to best contender)? root value : keep best value
+     */
     public int closestValue(TreeNode root, double target) {
-        // do a binary search, keeping track of the last value
         if (root == null) {
-            return (int) lastValue;
-        }
-        else if(root.val == target) {
+            return (int) bestValue;
+        } else if (root.val == target) {
             return root.val;
-        }
-        else if (target < root.val) {
-            if(root.left == null) {
-                // if root is a right node, and going left is null, either root or parent is the answer
-                if(!left) {
-                    return Math.abs(target - root.val) > Math.abs(target - lastValue)? (int) lastValue : root.val;
-                }
-                // if root is a left node, and going left is null, then target < root < parent, so root must be closest
-                else
-                    return root.val;
-            }
-            left = true;
-            lastValue = root.val;
+        } else if (target < root.val) {
+            bestValue = (Math.abs(target - root.val) < Math.abs(target - bestValue) ? root.val : bestValue);
             return closestValue(root.left, target);
-        }
-        else {
-            if (root.right == null) {
-                if(left) {
-                    return Math.abs(target - root.val) > Math.abs(target - lastValue)? (int) lastValue : root.val;
-                }
-                else
-                    return root.val;
-            }
-            left = false;
-            lastValue = root.val;
+        } else {
+            bestValue = (Math.abs(target - root.val) < Math.abs(target - bestValue) ? root.val : bestValue);
             return closestValue(root.right, target);
         }
     }
