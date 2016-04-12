bb793bbc5e5724086488874469d96fc9541dfd48
Michael J. Chen <bleriotusa@gmail.com>
2016-04-12 01:25:52 -0700

    LongestSubstring2Distinct working for some test cases

--- src/LongestSubstring2Distinct/LongestSubstring2Distinct.java
+++ src/LongestSubstring2Distinct/LongestSubstring2Distinct.java
@@ -1,8 +1,6 @@
 package LongestSubstring2Distinct;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by Michael J Chen on 4/10/16.
@@ -15,35 +13,52 @@ import java.util.Set;
  T is "ece" which its length is 3.
  */
 public class LongestSubstring2Distinct {
-    Set<Character> set = new LinkedHashSet<>();
+    Map<Character, Integer> map;
+    Deque<Character> queue;
+    char previous;
 
     /**
      *
      * @param s
      * @return
      */
-    public  int lengthOfLongestSubstringTwoDistinct(String s) {
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        map = new LinkedHashMap<>();
+        queue = new LinkedList<>();
         int current = 0;
         int max = 0;
 
         for(char c : s.toCharArray()) {
-            if(set.contains(c)) {
+            if(map.keySet().contains(c)) {
+                if(c == previous)
+                    map.put(c, map.get(c)+1);
+                else
+                    map.put(c, 1);
                 current++;
                 max = (current > max)? current : max;
             }
 
             else {
-                if(set.size() < 2) {
-                    set.add(c);
+                if(map.size() < 2) {
+                    map.put(c, 1);
                     current++;
                     max = (current > max)? current : max;
                 }
                 else {
+                    current = map.get(previous) + 1;
+                    max = (current > max)? current : max;
+                    map.remove(queue.peekFirst());
+                    map.put(c, 1);
+
                 }
             }
+            previous = c;
+            if(queue.size() == 2)
+                queue.removeFirst();
+            queue.add(c);
         }
 
-        return 0;
+        return max;
     }
 
 }
--- src/LongestSubstring2Distinct/LongestSubstring2DistinctTest.java
+++ src/LongestSubstring2Distinct/LongestSubstring2DistinctTest.java
@@ -9,8 +9,10 @@ import static org.junit.Assert.*;
  * Created by Home on 4/10/16.
  */
 public class LongestSubstring2DistinctTest {
-    String test1 = "aaaaaebeaaaaaaaa";
-    String test2 = "aaaaaebeaaaaaaaa"
+    String test1 = "abaca";
+    String test2 = "aaaaaebeaaaaaaaa";
+    String test3 = "eceba";
+    String test4 = "abcabcabc";
     LongestSubstring2Distinct tester;
 
     @Before
@@ -21,6 +23,13 @@ public class LongestSubstring2DistinctTest {
     @Test
     public void testLengthOfLongestSubstringTwoDistinct() throws Exception {
         assertEquals(3, tester.lengthOfLongestSubstringTwoDistinct(test1));
+        assertEquals(9, tester.lengthOfLongestSubstringTwoDistinct(test2));
+        assertEquals(3, tester.lengthOfLongestSubstringTwoDistinct(test3));
+        assertEquals(2, tester.lengthOfLongestSubstringTwoDistinct(test4));
+
+
+
+
     }
 
 }
