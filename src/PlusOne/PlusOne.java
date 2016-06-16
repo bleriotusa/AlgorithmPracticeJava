35143cf921ca3139c0cee72ca7a3f364b5292100
Michael J. Chen <bleriotusa@gmail.com>
2016-06-16 00:49:17 -0700

    Formatting

--- src/PlusOne/PlusOne.java
+++ src/PlusOne/PlusOne.java
@@ -1,32 +1,32 @@
 package PlusOne;
 
 import java.util.Arrays;
-import java.util.ListIterator;
 
 /**
+ * LeetCode's PlusOne problem. Given an int array representing an integer, add one to it and return.
  * Created by Michael on 6/13/2016.
  */
 public class PlusOne {
     public static int[] plusOne(int[] digits) {
 
-        if(digits[digits.length-1] == 9)
+        if (digits[digits.length - 1] == 9)
             digits = carry(digits);
         else
-            digits[digits.length-1]++;
+            digits[digits.length - 1]++;
 
 
         return digits;
-
     }
 
     /**
      * Shifts digits to the right one and adds a 1 to the front
+     *
      * @param digits Array to shift digits in
      */
     public static int[] shiftArray(int[] digits) {
         int[] newArray = Arrays.copyOf(digits, digits.length + 1);
-        for(int i = digits.length-1; i >= 0; i--)  {
-            newArray[i+1] = digits[i];
+        for (int i = digits.length - 1; i >= 0; i--) {
+            newArray[i + 1] = digits[i];
         }
         newArray[0] = 1;
         return newArray;
@@ -36,19 +36,18 @@ public class PlusOne {
         int curr = 0;
         boolean carry = true;
         // iterate through the list in reverse order
-        for(int i = digits.length-1; i >= 0 && carry; i--) {
+        for (int i = digits.length - 1; i >= 0 && carry; i--) {
             carry = false;
             curr = digits[i];
             // for each number, add one to it, passing up the digit if overflow
-            if(curr == 9) {
+            if (curr == 9) {
                 digits[i] = 0;
                 carry = true;
-            }
-            else
+            } else
                 digits[i]++;
 
             // if we're at the most significant digit, move everything over and add a one
-            if(carry && i==0) {
+            if (carry && i == 0) {
                 digits = shiftArray(digits);
 
             }
