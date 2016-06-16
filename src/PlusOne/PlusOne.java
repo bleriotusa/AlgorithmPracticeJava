8c652eafc886eb64d3aa98a55935a7d1629f13ea
Michael J. Chen <bleriotusa@gmail.com>
2016-06-16 00:47:31 -0700

    More cases for PlusOne. Seems like the algorithm could become more simplified, with a more generic core reused throughout the routine.

--- src/PlusOne/PlusOne.java
+++ src/PlusOne/PlusOne.java
@@ -1,5 +1,6 @@
 package PlusOne;
 
+import java.util.Arrays;
 import java.util.ListIterator;
 
 /**
@@ -9,9 +10,9 @@ public class PlusOne {
     public static int[] plusOne(int[] digits) {
 
         if(digits[digits.length-1] == 9)
-            carry(digits);
-
-        digits[digits.length-1]++;
+            digits = carry(digits);
+        else
+            digits[digits.length-1]++;
 
 
         return digits;
@@ -22,18 +23,21 @@ public class PlusOne {
      * Shifts digits to the right one and adds a 1 to the front
      * @param digits Array to shift digits in
      */
-    public static void shiftArray(int[] digits) {
+    public static int[] shiftArray(int[] digits) {
+        int[] newArray = Arrays.copyOf(digits, digits.length + 1);
         for(int i = digits.length-1; i >= 0; i--)  {
-            digits[i+1] = digits[i];
+            newArray[i+1] = digits[i];
         }
-        digits[0] = 1;
+        newArray[0] = 1;
+        return newArray;
     }
 
-    public static void carry(int[] digits) {
+    public static int[] carry(int[] digits) {
         int curr = 0;
-        boolean carry = false;
+        boolean carry = true;
         // iterate through the list in reverse order
-        for(int i = digits.length-1; i >= 0; i--) {
+        for(int i = digits.length-1; i >= 0 && carry; i--) {
+            carry = false;
             curr = digits[i];
             // for each number, add one to it, passing up the digit if overflow
             if(curr == 9) {
@@ -43,11 +47,12 @@ public class PlusOne {
             else
                 digits[i]++;
 
-            if(carry) {
-                // if we're at the most significant digit, move everything over and add a one
-                shiftArray(digits);
+            // if we're at the most significant digit, move everything over and add a one
+            if(carry && i==0) {
+                digits = shiftArray(digits);
 
             }
         }
+        return digits;
     }
 }
--- src/PlusOne/PlusOneTest.java
+++ src/PlusOne/PlusOneTest.java
@@ -3,6 +3,10 @@ package PlusOne;
 import com.sun.org.apache.xpath.internal.SourceTree;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 import static org.junit.Assert.*;
 
 /**
@@ -11,12 +15,24 @@ import static org.junit.Assert.*;
 public class PlusOneTest {
     @Test
     public void plusOne() throws Exception {
-        int[] x = {1,2,3};
-        int[] solution = {1,2,4};
-        int[] answer = PlusOne.plusOne(x);
-        for (int i = 0; i < answer.length; i++) {
-            System.out.println(String.format("%d: Sol = %d, Ans = %d", i, solution[i], answer[i]));
-            assertEquals(solution[i], answer[i]);
+        ArrayList<int[]> problemSolutions = new ArrayList<>();
+
+        int[] x1 = {1,2,3};
+        int[] sol1 = {1,2,4};
+        int[] x2 = {1,2,9};
+        int[] sol2 = {1,3,0};
+        int[] x3 = {9,9,9};
+        int[] sol3 = {1,0,0,0};
+        problemSolutions.addAll(Arrays.asList(x1, sol1, x2, sol2, x3, sol3));
+
+        // run through all the problems
+        for(int j = 0; j < problemSolutions.size(); j+=2) {
+            int[] answer = PlusOne.plusOne(problemSolutions.get(j));
+            for (int i = 0; i < answer.length; i++) {
+                System.out.println(String.format("%d: Sol = %d, Ans = %d", i, problemSolutions.get(j+1)[i], answer[i]));
+                assertEquals(problemSolutions.get(j+1)[i], answer[i]);
+            }
+            System.out.println("\n");
         }
     }
 
