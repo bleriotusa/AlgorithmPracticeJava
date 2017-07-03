4e5a36efc69085b5aa9f5bb97b3684e81cf553d7
Michael J Chen <bleriotusa@gmail.com>
2017-07-02 17:20:04 -0700

    Finished SortAlmostSortedArray using a min heap

--- JavaEPI/src/SortAlmostSortedArray/SortAlmostSortedArray.java
+++ JavaEPI/src/SortAlmostSortedArray/SortAlmostSortedArray.java
@@ -1,21 +1,49 @@
 package SortAlmostSortedArray;
 
-import java.util.Iterator;
+import java.util.*;
 
 /**
- * Created by MichaelLeisure on 6/25/17.
+ * Created by Michael on 6/25/17.
+ *
+ * Given a sequence of integers, where each integer is at most k away from it's sorted position,
+ * write a function that returns the sequence in the sorted order (original problem has us print the value)
  */
-public class SortAlmostSortedArray {
+class SortAlmostSortedArray {
 
     /**
      * Given a sequence of integers, where each integer is at most k away from it's sorted position,
-     * write a function that returns the sequence in the sorted order.
+     * write a function that returns the sequence in the sorted order (original problem has us print the value)
      *
      * [3, 1, 2, 4, 15, 6, 9, 11] --> [1, 2, 3, 4, 6, 9, 11, 15]
-     * @param sequence
-     * @param k
+     * @param sequence list of integers that are at most k spaces away from their sorted positions
+     * @param k: number of spaces that an unsorted element can be from it's sorted position
      */
-    public static void sortAlmostSortedArray(Iterator<Integer> sequence, int k){
+     static List<Integer> sortAlmostSortedArray(Iterator<Integer> sequence, int k){
+        List<Integer> sortedArray = new ArrayList<>();
+        PriorityQueue<Integer> heap = new PriorityQueue<>(k);
 
+
+
+        // iterate through the sequence, and then iterate through the rest of the heap once we're done with sequence
+        while(sequence.hasNext() || !heap.isEmpty()) {
+
+            if (sequence.hasNext())
+                heap.add(sequence.next());
+
+            System.out.println(Arrays.toString(heap.toArray()));
+
+            // Once the heap is filled to size k, that means any number that was k away from it's sorted
+            // position should be back into sorted position once it's pulled out of the heap.
+            //
+            // Therefore, pull out of the heap and add to the sorted list.
+            //
+            // Empty the heap out once we finish the sequence
+            //
+            if(heap.size() == k || !sequence.hasNext())
+                sortedArray.add(heap.remove());
+
+        }
+
+        return sortedArray;
     }
 }
--- JavaEPI/src/SortAlmostSortedArray/SortAlmostSortedArrayTest.java
+++ JavaEPI/src/SortAlmostSortedArray/SortAlmostSortedArrayTest.java
@@ -1,9 +1,24 @@
 package SortAlmostSortedArray;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.Assert.*;
+
+
+import static SortAlmostSortedArray.SortAlmostSortedArray.sortAlmostSortedArray;
+
 /**
  * Created by MichaelLeisure on 6/25/17.
  */
 public class SortAlmostSortedArrayTest {
+    public static List<Integer> arrayToList(int[] array) {
+        List<Integer> l = new ArrayList<>(array.length);
+        for(Integer num: array)
+            l.add(num);
+        return l;
+    }
+
     @org.junit.Before
     public void setUp() throws Exception {
 
@@ -12,13 +27,37 @@ public class SortAlmostSortedArrayTest {
     @org.junit.Test
     public void sortAlmostSortedArrayTest() throws Exception {
 //        List<Integer> sequence = Stream.of(3, 1, 2, 4, 15, 6, 9, 11).collect(Collectors.toList());
-        int[] sequence = new int[]{1,2,3};
+        int[] sequence1 = new int[]{3, 1, 2, 4, 15, 6, 9, 11};
+        List<Integer> seqList1 = arrayToList(sequence1);
 
-        for(Integer num: sequence)
-            System.out.println(num);
+        int[] solution1 = new int[]{1, 2, 3, 4, 6, 9, 11, 15};
+        List<Integer> solList1 = arrayToList(solution1);
 
 
-//        sortAlmostSortedArray(sequence.iterator(), 3);
+        for(Integer num: sortAlmostSortedArray(seqList1.iterator(), 3))
+            System.out.print(num + " ");
+
+        assertEquals(sortAlmostSortedArray(seqList1.iterator(), 3), solList1);
+
+        System.out.println("");
+    }
+
+    @org.junit.Test
+    public void sortAlmostSortedArrayTest2() throws Exception {
+        int k = 2;
+        int[] sequence1 = new int[]{3, -1, 2, 6, 4, 5, 8};
+        List<Integer> seqList1 = arrayToList(sequence1);
+
+        int[] solution1 = new int[]{-1, 2, 3, 4, 5, 6, 8};
+        List<Integer> solList1 = arrayToList(solution1);
+
+
+        for(Integer num: sortAlmostSortedArray(seqList1.iterator(), k))
+            System.out.print(num + " ");
+
+        assertEquals(sortAlmostSortedArray(seqList1.iterator(), k), solList1);
+
+        System.out.println("");
     }
 
 }
