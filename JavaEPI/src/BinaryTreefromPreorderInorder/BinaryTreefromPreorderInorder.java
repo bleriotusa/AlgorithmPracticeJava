94767ed24a158cdfb63d9299e630e6c5a8afc827
Michael J Chen <bleriotusa@gmail.com>
2017-07-12 00:08:29 -0700

    Quadratic time solution for tree reconstruction. Need to use a hashmap somehow to get it down to linear time.

--- JavaEPI/src/BinaryTreefromPreorderInorder/BinaryTreefromPreorderInorder.java
+++ JavaEPI/src/BinaryTreefromPreorderInorder/BinaryTreefromPreorderInorder.java
@@ -2,7 +2,6 @@ package BinaryTreefromPreorderInorder;
 
 import common.BinaryTreeNode;
 
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -15,54 +14,46 @@ import java.util.List;
  *   20           70      123           200
  *      \                                   \
  *        23                                223
+ *
  */
-public class BinaryTreefromPreorderInorder {
-    static BinaryTreeNode<Integer> binaryTreefromPreorderInorder(List<Character> preorder, List<Character> inorder) {
-//        BinaryTreeNode processingNode = binaryTree;
+class BinaryTreefromPreorderInorder {
 
-        Iterator<Character> porder = preorder.iterator();
-        Iterator<Character> iorder = inorder.iterator();
+    /**
+     * @param preorder List of nodes from a preorder traversal
+     * @param inorder  List of nodes from an inorder traversal
+     * @param <T>      Any type that can be compared
+     * @return a Binary Tree reconstructed using the two traversal orders.
+     */
+    static <T> BinaryTreeNode<T> binaryTreefromPreorderInorder(List<T> preorder, List<T> inorder) {
+        // base case 1: if traversals are empty, tree is non-existant
+        if (preorder.isEmpty() || inorder.isEmpty())
+            return null;
 
-        char pNext = porder.next();
-        char iNext = iorder.next();
+            // base case 2: if both lists have just one equal element, return that as the entire tree.
+        else if (preorder.size() == 1 && inorder.size() == 1 && preorder.get(0).equals(inorder.get(0)))
+            return new BinaryTreeNode<>(preorder.get(0));
 
-        BinaryTreeNode binaryTree, processingNode = binaryTree = new BinaryTreeNode(pNext);
-
-        /** create new left node
-         * traverse down until we meet the inorder node
-         * then traverse inorder back up the tree until we don't match (if parent doesn't match, go right)
-         *
+        /* else
+        1. Create the root node using the first of the preorder list
+        2. Create the left node with a recursive call using the k left nodes derived by the position of the root
+            in the inorder list
+        3. Create the right node with a recursive call using the nodes from k+1 to list.size()
          */
-        while (porder.hasNext() || iorder.hasNext()) {
 
+        T rootValue = preorder.get(0);
+        BinaryTreeNode<T> binaryTree = new BinaryTreeNode<>(rootValue);
+        int kLeftNodes = inorder.indexOf(rootValue) + 1;
 
-            while (porder.hasNext() && pNext != iNext) {
-                pNext = porder.next();
+        binaryTree.left = binaryTreefromPreorderInorder(preorder.subList(1, kLeftNodes), inorder.subList(0, kLeftNodes));
 
-                processingNode.left = new BinaryTreeNode(pNext);
-                processingNode = processingNode.left;
-
-            }
-
-            iNext = iorder.next();
-
-        }
-
-
+        // if there are no nodes in the right side of the list, attach nothing
+        // else attach a recursive call of the rest of the list
+        binaryTree.right = (kLeftNodes > inorder.size() - 1) ? null : binaryTreefromPreorderInorder(
+                preorder.subList(kLeftNodes, preorder.size()),
+                inorder.subList(kLeftNodes, inorder.size()));
 
         return binaryTree;
     }
+
 }
 
-/**
- case 1:
-            D
-
-         B    E
-      A    C
- [D, B, A, C, E]
- [A, B, C, D, E]
-
-
-
- */
--- JavaEPI/src/BinaryTreefromPreorderInorder/BinaryTreefromPreorderInorderTest.java
+++ JavaEPI/src/BinaryTreefromPreorderInorder/BinaryTreefromPreorderInorderTest.java
@@ -2,6 +2,9 @@ package BinaryTreefromPreorderInorder;
 import static BinaryTreefromPreorderInorder.BinaryTreefromPreorderInorder.binaryTreefromPreorderInorder;
 
 import common.BinaryTreeNode;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -13,18 +16,53 @@ import static org.junit.jupiter.api.Assertions.*;
  * Created by MichaelLeisure on 7/4/17.
  */
 class BinaryTreefromPreorderInorderTest {
+    int x;
+    @AfterEach
+    public void spaceTest() {
+        x = 5;
+        System.out.println('\n');
+    }
+
     @Test
     void binaryTreefromPreorderInorderTest() {
-        BinaryTreeNode a = new BinaryTreeNode<Integer>(10, new BinaryTreeNode<>(20), new BinaryTreeNode<>(30));
-//        BinaryTreeNode.printTree(a);
-//        int i=3, j  = 5;
-//        System.out.println(String.format("%d, %d", i, j));
+        /**
+         case 1:
+                D
 
+             B       E
+         A    C
+         [D, B, A, C, E]
+         [A, B, C, D, E]
+
+
+
+         */
         List<Character> preorder = Arrays.asList('D', 'B', 'A', 'C', 'E');
         List<Character> inorder = Arrays.asList('A', 'B', 'C', 'D', 'E');
         BinaryTreeNode result = binaryTreefromPreorderInorder(preorder, inorder);
 
         BinaryTreeNode.printTree(result);
+
+    }
+
+    @Test
+    void binaryTreefromPreorderInorderTest2() {
+        /**
+         *                  100
+         *              /           \
+         *          50                  175
+         *       /      \            /      \
+         *   20           70      123           200
+         *      \                                   \
+         *        23                                223
+         */
+
+        List<Integer> preorder = Arrays.asList(100, 50, 20, 23, 70, 175, 123, 200, 223);
+        List<Integer> inorder = Arrays.asList(20, 23, 50, 70, 100, 123, 175, 200, 223);
+        BinaryTreeNode<Integer> result = binaryTreefromPreorderInorder(preorder, inorder);
+
+        BinaryTreeNode.printTree(result);
+
     }
 
 }
